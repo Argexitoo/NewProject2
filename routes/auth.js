a19250349d8970e05107d0f463b2b933d43ebdc9
Argexitoo <argexitoo@gmail.com>
2021-12-21 20:46:48 +0100

    sign up

---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+const bcrypt = require('bcryptjs');
+const router = require('express').Router();
+
+// router.get('/sign-up', (req, res, next) => {
+//   res.render('/sign-up');
+// });
+
+module.exports = baseRoutes;
