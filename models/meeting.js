dfd125da54b80d7d0f6edff193e8356184c8f58c
xavireina <xavierreina93@gmail.com>
2022-01-10 20:00:14 +0100

    newmeetings

---
+++ models/meeting.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const meetingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  hour: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+   description: {
+    type: String,
+    required: true,
+  },
+  owner: { type: mongoose.Schema.ObjectId, ref: 'User'}
+});
+
+const Meeting = mongoose.model('Meeting', meetingSchema);
+
+module.exports = Meeting;
--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Dog = require('../models/dog');
 const User = require('../models/user');
+const Meeting = require('../models/meeting');
 
 function profileRoutes() {
   const router = express.Router();
@@ -104,6 +105,80 @@ function profileRoutes() {
       next(e);
     }
   });
+
+  //
+
+  router.get('/mymeetings', async (req, res, next) => {
+    const userId = req.session.currentUser._id;
+    try {
+      const foundMeetings = await Meeting.find({ owner: userId });
+      res.render('./profile/mymeetings', { foundMeetings });
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.get('/profile-newmeeting', async (req, res, next) => {
+    try {
+      res.render('./profile/profile-newmeeting');
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.post('/profile-newmeeting', async (req, res, next) => {
+    const userId = req.session.currentUser._id;
+    const { name, location, date, hour, description } = req.body;
+    try {
+      if (!name || !location || !date || !hour || !description) {
+        return res.render('./profile/profile-newmeeting', {
+          errorMessage: 'Complete all fields',
+        });
+      }
+      const meeting = await Meeting.create({ name, location, date, hour, description, owner: userId });
+      res.redirect('/mymeetings');
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.get('/profile-mymeeting/:id/edit', async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const editMeeting = await Meeting.findById(id);
+      res.render('./profile/update-form-meeting', { id, editMeeting });
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.post('/profile-mymeeting/:id/edit', async (req, res, next) => {
+    const { id } = req.params;
+    const { name, location, date, hour, description } = req.body;
+    try {
+      const editMeeting = await Meeting.findByIdAndUpdate(id, { name, location, date, hour, description }, { new: true });
+      return res.redirect('/mymeetings');
+    } catch (e) {
+      next(e);
+    }
+  });
+
+  router.get('/profile-mymeeting/:id/delete', async (req, res, next) => {
+    const { id } = req.params;
+    try {
+      const deleteMeeting = await Meeting.findByIdAndDelete(id);
+      res.redirect('/mymeetings');
+    } catch (e) {
+      next(e);
+    }
+  });
+
+
+
+
+
+
+
 
   return router;
 }
